56f8b600704d337640b87d9272b34b6ab197493f
uasd <54502184+Warek01@users.noreply.github.com>
2021-11-03 16:46:18 +0200

    Add files via upload

--- snake.c
+++ snake.c
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -5,34 +7,36 @@
 #include <stdint.h>
 #include <time.h>
 
+// For MSVC (VS) use _getchar and _kbhit
+// instead of getchar and kbhit
+// Also works for GNUC
+
 #if defined(WIN32) || defined(_WIN32)
-  #define CLS "cls"
-  #include <windows.h>
-#elif _POSIX_C_SOURCE >= 199309L
-  #define CLS "clear"
-#elif defined(__unix__) || defined(linux)
-  #define CLS "clear"
-  #include <unistd.h>
+#define CLS "cls"
+#include <windows.h>
+#else
+#define CLS "clear"
+#include <unistd.h>
 #endif
 
 typedef struct
 {
-  int x;
-  int y;
+	int x;
+	int y;
 } Position;
 
 typedef struct
 {
-  uint64_t start;
-  uint64_t end;
-  uint64_t diff;
+	uint64_t start;
+	uint64_t end;
+	uint64_t diff;
 } Timer;
 
 typedef struct
 {
-  Position head;
-  Position head_prev;
-  int ate;
+	Position head;
+	Position head_prev;
+	int ate;
 } Snake;
 
 typedef struct timespec Time;
@@ -41,6 +45,7 @@ typedef struct timespec Time;
 #define DOWN 's'
 #define LEFT 'a'
 #define RIGHT 'd'
+
 
 #define W 19
 #define H 9
@@ -55,374 +60,363 @@ Position food, center, body_nodes[W * H], byte_position;
 Timer timer;
 char key;
 int score = 0,
-    gameover = 0,
-    length = 0,
-    grid[W][H];
+gameover = 0,
+length = 0,
+grid[W][H];
 
 void nl(void);
 int randint(int, int);
 Position generate_food(int[W][H]);
 void sleep_ms(unsigned);
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-  srand(time(NULL));
-  timer.start = time(NULL);
+	srand(time(NULL));
+	timer.start = time(NULL);
 
-  for (int i = 0; i < W; i++)
-    for (int j = 0; j < W; j++)
-      grid[W][H] = NONE;
+	for (int i = 0; i < W; i++)
+		for (int j = 0; j < W; j++)
+			grid[W][H] = NONE;
 
-  // Center of grid
-  center.x = floor((double)W / 2);
-  center.y = floor((double)H / 2);
+	// Center of grid
+	center.x = floor((double)W / 2);
+	center.y = floor((double)H / 2);
 
-  // Start at center
-  grid[center.x][center.y] = HEAD;
+	// Start at center
+	grid[center.x][center.y] = HEAD;
 
-  snake.head.x = center.x;
-  snake.head.y = center.y;
-  snake.head_prev.x = 0;
-  snake.head_prev.y = 0;
-  snake.ate = 0;
+	snake.head.x = center.x;
+	snake.head.y = center.y;
+	snake.head_prev.x = 0;
+	snake.head_prev.y = 0;
+	snake.ate = 0;
 
-  food = generate_food(grid);
+	food = generate_food(grid);
 
-  // Initial food position
-  grid[food.x][food.y] = FOOD;
+	// Initial food position
+	grid[food.x][food.y] = FOOD;
 
-  system(CLS);
+	system(CLS);
 
-  // Top ##########
-  for (int i = 0; i < W + 2; i++)
-    printf("# ");
+	// Top ##########
+	for (int i = 0; i < W + 2; i++)
+		printf("# ");
 
-  nl();
+	nl();
 
-  for (int i = 0; i < H; i++)
-  {
-    // Left bound
-    putchar('#');
+	for (int i = 0; i < H; i++)
+	{
+		// Left bound
+		putchar('#');
 
-    for (int j = 0; j < W; j++)
-    {
-      if (grid[j][i] == NONE)
-        printf("  ");
-      else if (grid[j][i] == FOOD)
-        printf(" @");
-      else if (grid[j][i] == HEAD)
-        printf(" 1");
-      else if (grid[j][i] == BODY)
-        printf(" 0");
-    }
+		for (int j = 0; j < W; j++)
+		{
+			if (grid[j][i] == NONE)
+				printf("  ");
+			else if (grid[j][i] == FOOD)
+				printf(" @");
+			else if (grid[j][i] == HEAD)
+				printf(" 1");
+			else if (grid[j][i] == BODY)
+				printf(" 0");
+		}
 
-    // Right bound
-    printf(" #");
-    nl();
-  }
+		// Right bound
+		printf(" #");
+		nl();
+	}
 
-  // Bottom ############
-  for (int i = 0; i < W + 2; i++)
-    printf("# ");
+	// Bottom ############
+	for (int i = 0; i < W + 2; i++)
+		printf("# ");
 
-  printf("\nSCORE: %d\n", score);
-  printf("HEAD-X: %d  HEAD-Y: %d\n", snake.head.x, snake.head.y);
-  printf("FOOD-X: %d  FOOD-Y: %d\n", food.x, food.y);
+	printf("\nSCORE: %d\n", score);
+	printf("HEAD-X: %d  HEAD-Y: %d\n", snake.head.x, snake.head.y);
+	printf("FOOD-X: %d  FOOD-Y: %d\n", food.x, food.y);
 
-  nl();
+	nl();
 
-  key = getch();
+	key = _getch();
 
-  if (key == 'q')
-    exit(EXIT_SUCCESS);
+	if (key == 'q')
+		exit(EXIT_SUCCESS);
 
-  while (!gameover)
-  {
-    sleep_ms(UPDATE_DELAY);
-    system(CLS);
+	while (!gameover)
+	{
+		sleep_ms(UPDATE_DELAY);
+		system(CLS);
 
-    if (kbhit())
-      key = getch();
+		if (_kbhit())
+			key = _getch();
 
-    if (key == 'q')
-    {
-      gameover = 1;
-      break;
-    }
+		if (key == 'q')
+		{
+			gameover = 1;
+			break;
+		}
 
-    snake.head_prev.y = snake.head.y;
-    snake.head_prev.x = snake.head.x;
+		snake.head_prev.y = snake.head.y;
+		snake.head_prev.x = snake.head.x;
 
-    switch (key)
-    {
-    case UP:
-      grid[snake.head.x][snake.head.y] = 0;
+		switch (key)
+		{
+		case UP:
+			grid[snake.head.x][snake.head.y] = 0;
 
-      if (snake.head.y > 0)
-      {
-        grid[snake.head.x][snake.head.y - 1] = HEAD;
-        snake.head.y--;
-      }
-      else
-      {
-        grid[snake.head.x][H - 1] = HEAD;
-        snake.head.y = H - 1;
-      }
+			if (snake.head.y > 0)
+			{
+				grid[snake.head.x][snake.head.y - 1] = HEAD;
+				snake.head.y--;
+			}
+			else
+			{
+				grid[snake.head.x][H - 1] = HEAD;
+				snake.head.y = H - 1;
+			}
 
-      break;
+			break;
 
-    case DOWN:
-      grid[snake.head.x][snake.head.y] = NONE;
+		case DOWN:
+			grid[snake.head.x][snake.head.y] = NONE;
 
-      if (snake.head.y < H - 1)
-      {
-        grid[snake.head.x][snake.head.y + 1] = HEAD;
-        snake.head.y++;
-      }
-      else
-      {
-        grid[snake.head.x][snake.head.y] = NONE;
-        grid[snake.head.x][0] = 1;
-        snake.head.y = 0;
-      }
-      break;
+			if (snake.head.y < H - 1)
+			{
+				grid[snake.head.x][snake.head.y + 1] = HEAD;
+				snake.head.y++;
+			}
+			else
+			{
+				grid[snake.head.x][snake.head.y] = NONE;
+				grid[snake.head.x][0] = 1;
+				snake.head.y = 0;
+			}
+			break;
 
-    case RIGHT:
-      grid[snake.head.x][snake.head.y] = NONE;
-      if (snake.head.x < W - 1)
-      {
-        grid[snake.head.x + 1][snake.head.y] = HEAD;
-        snake.head.x++;
-      }
-      else
-      {
-        grid[0][snake.head.y] = HEAD;
-        snake.head.x = 0;
-      }
-      break;
-    case LEFT:
-      grid[snake.head.x][snake.head.y] = NONE;
-      if (snake.head.x > 0)
-      {
-        grid[snake.head.x - 1][snake.head.y] = HEAD;
-        snake.head.x--;
-      }
-      else
-      {
-        grid[W - 1][snake.head.y] = HEAD;
-        snake.head.x = W - 1;
-      }
-      break;
-    }
+		case RIGHT:
+			grid[snake.head.x][snake.head.y] = NONE;
+			if (snake.head.x < W - 1)
+			{
+				grid[snake.head.x + 1][snake.head.y] = HEAD;
+				snake.head.x++;
+			}
+			else
+			{
+				grid[0][snake.head.y] = HEAD;
+				snake.head.x = 0;
+			}
+			break;
+		case LEFT:
+			grid[snake.head.x][snake.head.y] = NONE;
+			if (snake.head.x > 0)
+			{
+				grid[snake.head.x - 1][snake.head.y] = HEAD;
+				snake.head.x--;
+			}
+			else
+			{
+				grid[W - 1][snake.head.y] = HEAD;
+				snake.head.x = W - 1;
+			}
+			break;
+		}
 
-    if (snake.ate)
-    {
-      // 0 body Nodes (create it on grid)
-      Position pos = {snake.head_prev.x, snake.head_prev.y};
-      body_nodes[length] = pos;
-      length++;
+		if (snake.ate)
+		{
+			Position pos = { snake.head_prev.x, snake.head_prev.y };
+			body_nodes[length] = pos;
+			length++;
 
-      grid[snake.head_prev.x][snake.head_prev.y] = BODY;
-      snake.ate = 0;
-    }
-    else if (length > 0)
-    {
-      int x, y, tx, ty;
+			grid[snake.head_prev.x][snake.head_prev.y] = BODY;
+			snake.ate = 0;
+		}
 
-      x = body_nodes[0].x;
-      y = body_nodes[0].y;
-      body_nodes[0].x = snake.head_prev.x;
-      body_nodes[0].y = snake.head_prev.y;
-      grid[body_nodes[0].x][body_nodes[0].y] = BODY;
+		if (length > 0)
+		{
+			int x, y, tx, ty;
 
-      if (length > 1)
-        for (int i = 1; i < length; i++)
-        {
-          tx = body_nodes[i].x;
-          ty = body_nodes[i].y;
+			x = body_nodes[0].x;
+			y = body_nodes[0].y;
+			body_nodes[0].x = snake.head_prev.x;
+			body_nodes[0].y = snake.head_prev.y;
+			grid[body_nodes[0].x][body_nodes[0].y] = BODY;
 
-          body_nodes[i].x = x;
-          body_nodes[i].y = y;
+			if (length > 1)
+				for (int i = 1; i < length; i++)
+				{
+					tx = body_nodes[i].x;
+					ty = body_nodes[i].y;
 
-          grid[body_nodes[i].x][body_nodes[i].y] = BODY;
+					body_nodes[i].x = x;
+					body_nodes[i].y = y;
 
-          x = tx;
-          y = ty;
-        }
-      grid[x][y] = NONE;
-    }
+					grid[body_nodes[i].x][body_nodes[i].y] = BODY;
 
-    for (int i = 0; i < length; i++)
-      if (snake.head.x == body_nodes[i].x && snake.head.y == body_nodes[i].y)
-      {
-        byte_position.x = snake.head.x;
-        byte_position.y = snake.head.y;
-        gameover = 1;
-      }
+					x = tx;
+					y = ty;
+				}
+			grid[x][y] = NONE;
+		}
 
-    // Top ########
-    for (int i = 0; i < W + 2; i++)
-      printf("# ");
+		for (int i = 0; i < length; i++)
+			if (snake.head.x == body_nodes[i].x && snake.head.y == body_nodes[i].y)
+			{
+				byte_position.x = snake.head.x;
+				byte_position.y = snake.head.y;
+				gameover = 1;
+			}
 
-    nl();
+		// Top ########
+		for (int i = 0; i < W + 2; i++)
+			printf("# ");
 
-    for (int i = 0; i < H; i++)
-    {
-      // Left bound
-      putchar('#');
+		nl();
 
-      for (int j = 0; j < W; j++)
-      {
-        if (grid[j][i] == NONE)
-          printf("  ");
-        else if (grid[j][i] == HEAD)
-          printf(" 1");
-        else if (grid[j][i] == FOOD)
-          printf(" @");
-        else if (grid[j][i] == BODY)
-          printf(" 0");
-      }
+		for (int i = 0; i < H; i++)
+		{
+			// Left bound
+			putchar('#');
 
-      // Right bound
-      printf(" #");
-      nl();
-    }
+			for (int j = 0; j < W; j++)
+			{
+				if (grid[j][i] == NONE)
+					printf("  ");
+				else if (grid[j][i] == HEAD)
+					printf(" 1");
+				else if (grid[j][i] == FOOD)
+					printf(" @");
+				else if (grid[j][i] == BODY)
+					printf(" 0");
+			}
 
-    // bottom ##########
-    for (int i = 0; i < W + 2; i++)
-      printf("# ");
+			// Right bound
+			printf(" #");
+			nl();
+		}
 
-    nl();
+		// bottom ##########
+		for (int i = 0; i < W + 2; i++)
+			printf("# ");
 
-    // On food pikup
-    if (snake.head.x == food.x && snake.head.y == food.y)
-    {
-      snake.ate = 1;
-      score++;
+		nl();
 
-      food = generate_food(grid);
-      grid[food.x][food.y] = FOOD;
-    }
+		// On food pikup
+		if (snake.head.x == food.x && snake.head.y == food.y)
+		{
+			snake.ate = 1;
+			score++;
 
-    switch (key)
-    {
-    case UP:
-      printf("UP");
-      break;
-    case DOWN:
-      printf("DOWN");
-      break;
-    case LEFT:
-      printf("LEFT");
-      break;
-    case RIGHT:
-      printf("RIGHT");
-      break;
+			food = generate_food(grid);
+			grid[food.x][food.y] = FOOD;
+		}
 
-    default:
-      break;
-    }
+		switch (key)
+		{
+		case UP:
+			printf("UP");
+			break;
+		case DOWN:
+			printf("DOWN");
+			break;
+		case LEFT:
+			printf("LEFT");
+			break;
+		case RIGHT:
+			printf("RIGHT");
+			break;
 
-    printf("  |  SCORE: %d\n", score);
-    printf("HEAD-X: %d  HEAD-Y: %d\n", snake.head.x, snake.head.y);
-    printf("FOOD-X: %d  FOOD-Y: %d\n", food.x, food.y);
+		default:
+			break;
+		}
 
-    nl();
-  }
+		printf("  |  SCORE: %d\n", score);
+		printf("HEAD-X: %d  HEAD-Y: %d\n", snake.head.x, snake.head.y);
+		printf("FOOD-X: %d  FOOD-Y: %d\n", food.x, food.y);
+	}
 
-  system("clear");
+	system("clear");
 
-  // ENDSCREEN ------------------
-  // Top ########
-  for (int i = 0; i < W + 2; i++)
-    printf("# ");
+	// ENDSCREEN ------------------
+	// Top ########
+	for (int i = 0; i < W + 2; i++)
+		printf("# ");
 
-  nl();
+	nl();
 
-  for (int i = 0; i < H; i++)
-  {
-    // Left bound
-    putchar('#');
+	for (int i = 0; i < H; i++)
+	{
+		// Left bound
+		putchar('#');
 
-    for (int j = 0; j < W; j++)
-    {
-      if (j == byte_position.x && i == byte_position.y)
-        printf(" X");
-      else if (grid[j][i] == NONE)
-        printf("  ");
-      else if (grid[j][i] == HEAD)
-        printf(" X");
-      else if (grid[j][i] == FOOD)
-        printf(" @");
-      else if (grid[j][i] == BODY)
-        printf(" 0");
-    }
+		for (int j = 0; j < W; j++)
+		{
+			if (j == byte_position.x && i == byte_position.y)
+				printf(" X");
+			else if (grid[j][i] == NONE)
+				printf("  ");
+			else if (grid[j][i] == HEAD)
+				printf(" X");
+			else if (grid[j][i] == FOOD)
+				printf(" @");
+			else if (grid[j][i] == BODY)
+				printf(" 0");
+		}
 
-    // Right bound
-    printf(" #");
-    nl();
-  }
+		// Right bound
+		printf(" #");
+		nl();
+	}
 
-  // bottom ##########
-  for (int i = 0; i < W + 2; i++)
-    printf("# ");
+	// bottom ##########
+	for (int i = 0; i < W + 2; i++)
+		printf("# ");
 
-  nl();
+	nl();
 
-  timer.end = time(NULL);
-  timer.diff = timer.end - timer.start;
+	timer.end = time(NULL);
+	timer.diff = timer.end - timer.start;
 
-  printf("\n\tGAME OVER\n\n");
-  printf("Score: %d  |  In-game time: %llu seconds\n", score, timer.diff);
-  printf("---------------------\n");
-  printf("Press any key to exit\n\n");
+	printf("\n\tGAME OVER\n\n");
+	printf("Score: %d  |  In-game time: %llu seconds\n", score, timer.diff);
+	printf("---------------------\n");
+	printf("Press any key to exit\n");
 
-  getch();
+	_getch();
 
-  exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
 
 // Newline
 void nl(void)
 {
-  printf("\n");
+	printf("\n");
 }
 
 int randint(int min, int max)
 {
-  return (rand() % (max - min + 1) + min);
+	return (rand() % (max - min + 1) + min);
 }
 
 // Generates random food coordinates which do not intersect given Head and Body of snake
 Position generate_food(int grid[W][H])
 {
-  int x, y;
-  do
-  {
-    x = randint(0, W - 1);
-    y = randint(0, H - 1);
-  } while (grid[x][y] != 0);
+	int x, y;
+	do
+	{
+		x = randint(0, W - 1);
+		y = randint(0, H - 1);
+	} while (grid[x][y] != 0);
 
-  Position food = {x, y};
+	Position food = { x, y };
 
-  return food;
+	return food;
 }
 
 void sleep_ms(unsigned int ms)
 {
-  #if defined(WIN32) || defined(_WIN32)
-    Sleep(ms);
-  #elif _POSIX_C_SOURCE >= 199309L
-    struct timespec delay = {0, ms * 1000 * 1000};
-    nanosleep(&delay, NULL);
-  #elif defined(__unix__) || defined(linux)
-    if (ms >= 1000)
-      sleep(ms / 1000);
-    usleep((ms % 1000) * 1000);
-  #else
-    struct timeval tv;
-    tv.tv_sec = milliseconds / 1000;
-    tv.tv_usec = milliseconds % 1000 * 1000;
-    select(0, NULL, NULL, NULL, &tv);
-  #endif
+#if defined(WIN32) || defined(_WIN32)
+	Sleep(ms);
+#else
+	struct timespec delay = { 0, ms * 1000 * 1000 };
+	nanosleep(&delay, NULL);
+#endif
 }
